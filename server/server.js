0bf459a326a665b7a2640310a08506a29e49484a
Frank Stepanski <fsjay@hotmail.com>
2021-05-06 13:47:11 -0400

    updates

--- server/server.js
+++ server/server.js
@@ -1,35 +1,59 @@
-const apiRouter = require("./routes");
+require('dotenv').config();
+
 const express = require("express");
 const cors = require("cors");
+const chalk = require("chalk");
+const compression = require('compression')
+const todosRouter = require("./routes/todos.router");
+const { connect } = require("./db/client");
+const { sync } = require("./db/sync"); 
+const { seed } = require("./db/seed"); 
+const { PORT = 8080 } = process.env;
+
 const app = express();
-const PORT = 8080;
 
-// express middleware:
-app.use(cors()); 
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.json());  
+const createApp = () => {
+  // middleware:
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true })); 
+  app.use(compression());
 
-/**
- * define route handler
- */
-app.use('/api/todos', apiRouter);
+  // auth and api routes
+  app.use("/api", todosRouter);
+}
 
-/**
- * express error handler
- */
+const syncDb = async() => {
+  // Eevironment variables are always strings
+  if (process.env.DBSYNC === "TRUE"){
+    console.log(`>>> Seeding database`);
+    await sync()
+    await seed()
+    console.log('>>> Seeding completed');
+  } else {
+    connect();
+  }
+}
 
-app.use((err, req, res, next) => {
-   const defaultErr = {
-     log: 'Express error handdler caught unknown middleware error',
-     status: 400,
-     message: { err: 'An error occurred' },
-   };
- 
-   const errorObj = Object.assign(defaultErr, err);
-   console.log('ERROR: ', errorObj.log);
-   return res.status(errorObj.status).json(errorObj.message);
- });
- 
-app.listen(PORT, () => {
-   console.log(`Server is listening on port ${PORT}`)
-});
+const startListening = () => {
+  app.listen(PORT, () =>
+     console.log(chalk.bgRedBright(`>>>Server is listening on port ${PORT}`))
+  )
+};
+
+/*
+ * Normally you have to "sync" you database the first time your server is run. 
+ * This will build your database and insert any initial data.
+ * But this will only happen once, so in prod applications, there will be separate
+ * scripts (.sql) that you will run outside of starting your server that will do this.
+ * This process is normally handled by a DBA, but sometimes it can be done by a developer 
+ * if the database is small enough.
+ *
+*/
+async function bootApp() {
+    await syncDb() // initializing and seeding your database
+    await createApp()
+    await startListening()
+ }
+
+bootApp();
